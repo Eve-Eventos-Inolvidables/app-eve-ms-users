07fc4948c9128fc0a4e4762a87f681656077f096
Math2okt <matias.marecos@fiuni.edu.py>
2026-09-23 00:17:28 -0300

    feat: files

--- src/main/java/com/example/appevemsusers/AppEveMsUsersApplication.java
+++ src/main/java/com/example/appevemsusers/AppEveMsUsersApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AppEveMsUsersApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AppEveMsUsersApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AppEveMsUsersApplication.class, args);
+    }
 
 }
