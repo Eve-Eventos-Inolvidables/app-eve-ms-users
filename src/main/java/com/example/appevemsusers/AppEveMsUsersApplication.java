b1c2741143896cb9cba922a3bb8178b3baee663a
Math2okt <matias.marecos@fiuni.edu.py>
2026-09-23 00:15:52 -0300

    feat: hello world

---
+++ src/main/java/com/example/appevemsusers/AppEveMsUsersApplication.java
@@ -0,0 +1,13 @@
+package com.example.appevemsusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEveMsUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppEveMsUsersApplication.class, args);
+	}
+
+}
